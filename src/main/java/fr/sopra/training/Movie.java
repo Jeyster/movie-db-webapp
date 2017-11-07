0a04df13c8aaa0af0ff7faba595dd0f874ae70e6
jeyster <gaurat.mathieu@orange.fr>
2017-11-07 15:01:58 +0100

    Tache 3 : Modèle de données
    Création des beans Studio, Movie, Realisator, Actor.

---
+++ src/main/java/fr/sopra/training/Movie.java
@@ -0,0 +1,52 @@
+package fr.sopra.training;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Movie {
+
+	private Integer id;
+	private String name;
+	private Date releaseDate;
+	private Studio studio = new Studio();
+	private Realisator realisator = new Realisator();
+	private List<Actor> actors = new ArrayList<>();
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public Studio getStudio() {
+		return studio;
+	}
+	public void setStudio(Studio studio) {
+		this.studio = studio;
+	}
+	public Realisator getRealisator() {
+		return realisator;
+	}
+	public void setRealisator(Realisator realisator) {
+		this.realisator = realisator;
+	}
+	public List<Actor> getActors() {
+		return actors;
+	}
+	public void setActors(List<Actor> actors) {
+		this.actors = actors;
+	}
+}
---
+++ src/main/java/fr/sopra/training/Studio.java
@@ -0,0 +1,29 @@
+package fr.sopra.training;
+
+import java.util.Date;
+
+public class Studio {
+	private Integer id;
+	private String name;
+	private Date creationDate;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	
+}
